f073b951996536507d9fad8085b866681b0f11d9
phamtanphat <phatdroid94@gmail.com>
2021-11-23 20:45:15 +0700

    Initial commit

---
+++ app/src/main/java/com/example/appsales26082021/Engine.java
@@ -0,0 +1,27 @@
+package com.example.appsales26082021;
+
+public class Engine {
+    private int horsepower;
+    private String name;
+
+    public Engine(int horsepower, String name) {
+        this.horsepower = horsepower;
+        this.name = name;
+    }
+
+    public int getHorsepower() {
+        return horsepower;
+    }
+
+    public void setHorsepower(int horsepower) {
+        this.horsepower = horsepower;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ app/src/main/java/com/example/appsales26082021/Wheel.java
@@ -0,0 +1,4 @@
+package com.example.appsales26082021;
+
+public class Wheel {
+}
