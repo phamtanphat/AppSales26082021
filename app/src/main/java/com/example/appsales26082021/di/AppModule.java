1e94620024493b00f9551b3de090aae7e2cea889
phamtanphat <phatdroid94@gmail.com>
2021-11-27 21:05:17 +0700

    module

---
+++ app/src/main/java/com/example/appsales26082021/di/AppModule.java
@@ -0,0 +1,49 @@
+package com.example.appsales26082021.di;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+import okhttp3.OkHttpClient;
+import okhttp3.Protocol;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+@Module
+public class AppModule {
+
+    @Provides
+    @Singleton
+    OkHttpClient provideClient(){
+        return new OkHttpClient.Builder()
+                .readTimeout(30 , TimeUnit.SECONDS)
+                .writeTimeout(30 , TimeUnit.SECONDS)
+                .connectTimeout(30 , TimeUnit.SECONDS)
+                .retryOnConnectionFailure(true)
+                .protocols(Arrays.asList(Protocol.HTTP_1_1))
+                .build();
+    }
+
+    @Provides
+    @Singleton
+    Gson provideGson(){
+        return new GsonBuilder().setLenient().create();
+    }
+
+    @Provides
+    @Singleton
+    Retrofit provideRetrofit(Gson gson , OkHttpClient client){
+        return new Retrofit.Builder()
+                .baseUrl("https://freeapi.code4func.com/")
+                .client(client)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+    }
+
+}
