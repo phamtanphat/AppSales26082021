2d89360fa1b8548bb8852ef4a7f30050f2662e35
phamtanphat <phatdroid94@gmail.com>
2021-12-02 21:27:57 +0700

    finish sign in

--- app/src/main/java/com/example/appsales26082021/view/auth/SignInActivity.java
+++ app/src/main/java/com/example/appsales26082021/view/auth/SignInActivity.java
@@ -1,13 +1,17 @@
 package com.example.appsales26082021.view.auth;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.appsales26082021.R;
 import com.example.appsales26082021.api.ResourceType;
+import com.example.appsales26082021.databinding.ActivitySignInBinding;
 import com.example.appsales26082021.model.UserModel;
 import com.example.appsales26082021.viewmodel.AuthViewModel;
 import com.example.appsales26082021.viewmodel.ViewModelFactoryProvider;
@@ -21,29 +25,49 @@ public class SignInActivity extends DaggerAppCompatActivity {
     @Inject
     public ViewModelFactoryProvider provider;
 
-    AuthViewModel authViewModel;
+    AuthViewModel mAuthViewModel;
+
+    private ActivitySignInBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_in);
+        mBinding = ActivitySignInBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
 
-        authViewModel = new ViewModelProvider(this,provider).get(AuthViewModel.class);
+        mBinding.toolbarSignIn.setTitle("Sign In");
+        mBinding.toolbarSignIn.setTitleTextColor(Color.WHITE);
 
-        authViewModel.getUserData().observe(this, new Observer<ResourceType<UserModel>>() {
+        mAuthViewModel = new ViewModelProvider(this, provider).get(AuthViewModel.class);
+
+        mAuthViewModel.getUserData().observe(this, new Observer<ResourceType<UserModel>>() {
             @Override
             public void onChanged(ResourceType<UserModel> userModelResourceType) {
-                if (userModelResourceType.status == ResourceType.Status.LOADING){
-                    Log.d("BBB","Loading");
-                }else if(userModelResourceType.status == ResourceType.Status.SUCCESS){
-                    Log.d("BBB",userModelResourceType.data.toString());
-                }else {
-                    Log.d("BBB",userModelResourceType.message);
+                if (userModelResourceType.status == ResourceType.Status.LOADING) {
+                    mBinding.includeLoading.layoutLoading.setVisibility(View.VISIBLE);
+                } else if (userModelResourceType.status == ResourceType.Status.SUCCESS) {
+                    Toast.makeText(SignInActivity.this, "Đăng nhập thành công", Toast.LENGTH_SHORT).show();
+                    mBinding.includeLoading.layoutLoading.setVisibility(View.GONE);
+                } else {
+                    Toast.makeText(SignInActivity.this, userModelResourceType.message, Toast.LENGTH_SHORT).show();
+                    mBinding.includeLoading.layoutLoading.setVisibility(View.GONE);
                 }
             }
         });
 
-        authViewModel.signIn(new UserModel("nguyenvana@gmail.com","12345678"));
+        mBinding.buttonSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = mBinding.textEditEmail.getText().toString();
+                String password = mBinding.textEditPassword.getText().toString();
+
+                if (email.isEmpty() || password.isEmpty()) {
+                    Toast.makeText(SignInActivity.this, "Bạn chưa nhập đủ thông tin!!!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                mAuthViewModel.signIn(new UserModel(email, password));
+            }
+        });
 
 
     }
