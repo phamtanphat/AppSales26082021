27fea030877418cd1c5d0ffa9fdc90f7d552c02b
phamtanphat <phatdroid94@gmail.com>
2021-12-02 20:48:17 +0700

    setup repository

--- app/src/main/java/com/example/appsales26082021/api/ApiRequest.java
+++ app/src/main/java/com/example/appsales26082021/api/ApiRequest.java
@@ -1,4 +1,12 @@
 package com.example.appsales26082021.api;
 
+import com.example.appsales26082021.model.UserModel;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
 public interface ApiRequest {
+    @POST("api/v1/user/sign-in")
+    Call<ResourceType<UserModel>> signIn(@Body UserModel userModel);
 }
--- app/src/main/java/com/example/appsales26082021/repository/AuthRepository.java
+++ app/src/main/java/com/example/appsales26082021/repository/AuthRepository.java
@@ -1,4 +1,22 @@
 package com.example.appsales26082021.repository;
 
+import com.example.appsales26082021.api.ApiRequest;
+import com.example.appsales26082021.api.ResourceType;
+import com.example.appsales26082021.model.UserModel;
+
+import javax.inject.Inject;
+
+import retrofit2.Call;
+
 public class AuthRepository {
+    private ApiRequest apiRequest;
+
+    @Inject
+    public AuthRepository(ApiRequest apiRequest){
+        this.apiRequest = apiRequest;
+    }
+
+    public Call<ResourceType<UserModel>> signIn(UserModel userModel){
+       return apiRequest.signIn(userModel);
+    }
 }
