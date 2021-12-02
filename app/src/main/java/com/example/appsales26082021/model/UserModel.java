28d8a0a190799424f189ebb1a9ee6ab33392ce42
phamtanphat <phatdroid94@gmail.com>
2021-12-02 20:39:09 +0700

    user model

---
+++ app/src/main/java/com/example/appsales26082021/model/UserModel.java
@@ -0,0 +1,92 @@
+package com.example.appsales26082021.model;
+
+public class UserModel {
+
+    private String userId;
+    private String fullName;
+    private String email;
+    private String password;
+    private String phone;
+    private String address;
+    private String role;
+    private String createdAt;
+    private String updatedAt;
+    private String token;
+
+    public UserModel(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(String updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
