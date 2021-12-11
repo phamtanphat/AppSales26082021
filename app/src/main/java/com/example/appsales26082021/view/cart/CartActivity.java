26a76b9b23975ab389f3d0d089421185f57f8832
phamtanphat <phatdroid94@gmail.com>
2021-12-11 20:56:54 +0700

    ui cart

---
+++ app/src/main/java/com/example/appsales26082021/view/cart/CartActivity.java
@@ -0,0 +1,16 @@
+package com.example.appsales26082021.view.cart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.appsales26082021.R;
+
+public class CartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart);
+    }
+}
